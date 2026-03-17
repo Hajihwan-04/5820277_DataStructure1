a947f29fbe5cc6e52daf38297b7c84955afbd000
하지환 <hach121213@naver.com>
2026-03-17 15:36:56 +0900

    2026.03.17 수업중 실행한 코드

---
+++ 2026.03.17/1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int c, char** v) {
+
+	printf("c: %d\n", c);
+	for (int i = 0; i < c; i++) {
+		printf("v%d: %s\n",i, v[i]);
+	}
+	int a = atoi(v[1]), b = atoi(v[2]);
+	printf("sum : %d", a + b);
+}
