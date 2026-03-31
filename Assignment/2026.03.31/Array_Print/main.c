36cb07057036691857b2992300586b078f729ea0
하지환 <hach121213@naver.com>
2026-03-31 22:12:46 +0900

    2026.03.26 과제

---
+++ Assignment/2026.03.31/Array_Print/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define SIZE 100
+
+int main() {
+	int arr[SIZE];
+
+	srand(time(NULL));
+	for (int i = 0; i < SIZE; i++) {
+		
+	}
+	
+	
+
+	return 0;
+}
