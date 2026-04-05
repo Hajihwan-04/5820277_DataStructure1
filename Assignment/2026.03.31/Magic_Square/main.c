3f6413e1c03fafe79baa95398319b1a5580ac413
하지환 <hach121213@naver.com>
2026-04-05 12:09:23 +0900

    마방진 수정

--- Assignment/2026.03.31/Magic_Square/main.c
+++ Assignment/2026.03.31/Magic_Square/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-#define N 11
+#define N 5
 
 int main() {
 	if (N % 2 == 0) {
 		printf("짝수는 불가"); return 1;
 	}
+	printf("[%d x %d 마방진]\n\n",N, N);
+	
 
 	int arr[N][N] = {0}, size = N * N;
 	int x = ((N - 1) / 2) - 1, y = 1;
@@ -21,7 +23,6 @@ int main() {
 			if (y > N - 1) { y -= N; }
 			if (x < 0) { x = N - 1; }
 		}
-		printf("%d, %d\n", y, x);
 		arr[y][x] = i;
 	}
 	int sumArrX[N] = { 0 }, sumArrY[N] = { 0 }, sumArrD[2] = { 0 };
@@ -42,13 +43,15 @@ int main() {
 		for (int dash = 0; dash < (N * 5 + 1); dash++) { printf("-"); }
 		printf("\n");
 	}
-		
-	for (int x = 0; x < N; x++) {
-		printf("가로 %d칸 합 : %d\n", y, sumArrY[y]);
-	}
+	
 	for (int y = 0; y < N; y++) {
-		printf("세로 %d칸 합 : %d\n",  x, sumArrX[x]);
+		printf("가로 %2d행 합 : %d\n",  y, sumArrY[y]);
 	}
+	printf("\n");
+	for (int x = 0; x < N; x++) {
+		printf("세로 %2d열 합 : %d\n", x, sumArrX[x]);
+	}
+	printf("\n");
 	printf("0, 0에서 %d, %d방향 대각선 : %d\n", N - 1, N - 1, sumArrD[0]);
 	printf("%d, 0에서 0, %d방향 대각선 : %d\n", N - 1, N - 1, sumArrD[1]);
 }
