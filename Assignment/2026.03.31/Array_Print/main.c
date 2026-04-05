38427ad432884137cbcf9fa6e1abad6a584b521a
하지환 <hach121213@naver.com>
2026-04-05 11:47:57 +0900

    배열 합, 마방진

--- Assignment/2026.03.31/Array_Print/main.c
+++ Assignment/2026.03.31/Array_Print/main.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-#define SIZE 100
+#define N 100
 
 int main() {
-	int arr[SIZE];
+	int arr[N], big = 0, small = N;
+	long sum = 0;
 
+	//N개의 배열에 임의의 수 채움
 	srand(time(NULL));
-	for (int i = 0; i < SIZE; i++) {
-		
+	for (int i = 0; i < N; i++) {
+		arr[i] = rand() % N;
 	}
-	
-	
+
+	printf("순서대로 출력\n");
+	for (int i = 0; i < N; i++) {
+		printf("|%3d: %3d|", i + 1, arr[i]);
+		if ((i + 1) % 5 == 0 && i != 0) printf("\n");
+	}
+
+	printf("--------------------------------------------------\n");
+
+	printf("뒤에서부터 출력\n");
+	for (int i = 99; i >= 0; i--) {
+		printf("|%3d: %3d|", i + 1, arr[i]);
+		if (i % 5 == 0 && i != 99) printf("\n");
+	}
+
+	for (int i = 0; i < 100; i++) {
+		sum += arr[i];
+		if (big < arr[i]) big = arr[i];
+		if (small > arr[i]) small = arr[i];
+	}
+
+	printf("--------------------------------------------------\n");
+
+	printf("배열총합 : %ld\n최대값   : %d\n최소값   : %d", sum, big, small);
 
 	return 0;
 }
---
+++ Assignment/2026.03.31/Magic_Square/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define N 11
+
+int main() {
+	if (N % 2 == 0) {
+		printf("짝수는 불가"); return 1;
+	}
+
+	int arr[N][N] = {0}, size = N * N;
+	int x = ((N - 1) / 2) - 1, y = 1;
+	for (int i = 1; i < size + 1; i++) {
+		y--;
+		if (y < 0) { y = N -1; }
+		x++;
+		if (x > N - 1) { x = 0; }
+
+		if (arr[y][x] != 0) {
+			y += 2;
+			x -= 1;
+			if (y > N - 1) { y -= N; }
+			if (x < 0) { x = N - 1; }
+		}
+		printf("%d, %d\n", y, x);
+		arr[y][x] = i;
+	}
+	int sumArrX[N] = { 0 }, sumArrY[N] = { 0 }, sumArrD[2] = { 0 };
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			printf("|%3d ", arr[i][j]);
+			
+			for (int x = 0; x < N; x++) {
+				if (j == x) { sumArrX[x] += arr[i][j]; }
+			}
+			for (int y = 0; y < N; y++) {
+				if (i == y) { sumArrY[y] += arr[i][j]; }
+			}
+			if (i == j) { sumArrD[0] += arr[i][j]; }
+			if (i + j == N - 1) { sumArrD[1] += arr[i][j]; }
+		}
+		printf("|\n");
+		for (int dash = 0; dash < (N * 5 + 1); dash++) { printf("-"); }
+		printf("\n");
+	}
+		
+	for (int x = 0; x < N; x++) {
+		printf("가로 %d칸 합 : %d\n", y, sumArrY[y]);
+	}
+	for (int y = 0; y < N; y++) {
+		printf("세로 %d칸 합 : %d\n",  x, sumArrX[x]);
+	}
+	printf("0, 0에서 %d, %d방향 대각선 : %d\n", N - 1, N - 1, sumArrD[0]);
+	printf("%d, 0에서 0, %d방향 대각선 : %d\n", N - 1, N - 1, sumArrD[1]);
+}
