8b16332c7e63b294ade0e2d0f6effd16be88070c
하지환 <hach121213@naver.com>
2026-04-22 22:51:31 +0900

    .

--- 5820277_DataStructure1/Main.c
+++ 5820277_DataStructure1/Main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int getFibo(int n) {
 	int f1 = 1, f2 = 1;
-	for (int i = 3; i < 20; i++) {
-		int fn = f1 + f2;
+	int fn = 1;
+	for (int i = 3; i <= n; i++) {
+		fn = f1 + f2;
 		f1 = f2;
 		f2 = fn;
 		printf("%d: %d\n", i, fn);
 	}
+	return fn;
+}
+
+int main() {
+	printf("%d", getFibo(2));
 	return 0;
 }
