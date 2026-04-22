915fb768a5462270abfb2ab2b011b9a963952420
하지환 <hach121213@naver.com>
2026-04-22 22:36:15 +0900

    .

--- 5820277_DataStructure1/Main.c
+++ 5820277_DataStructure1/Main.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 
 int main() {
-	char c[10];
-	scanf_s("%s", c, (unsigned)sizeof(c));
-	printf("%d", c[0]);
-
+	int f1 = 1, f2 = 1;
+	for (int i = 3; i < 20; i++) {
+		int fn = f1 + f2;
+		f1 = f2;
+		f2 = fn;
+		printf("%d: %d\n", i, fn);
+	}
 	return 0;
 }
